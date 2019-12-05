e5da3001ff0ea2e1b18f34f3c92bf03c4fbc5afe
Mengsen.Wang <mengsen_wang@163.com>
2019-12-05 21:18:55 +0800

    New function

--- OverloadOperators/MyInt.cpp
+++ OverloadOperators/MyInt.cpp
@@ -4,21 +4,22 @@
 //默认构造
 MyInt::MyInt()
 {
-	this->m_Num = new int;
-	*m_Num = 0;
+	this->m_Num = new int(0);
+	cout << "默认构造" << endl;
 }
 
 //含参构造
 MyInt::MyInt(int num)
 {
-	this->m_Num = new int;
-	*m_Num = num;
+	this->m_Num = new int(num);
+	cout << "含参构造" << endl;
 }
 
 //拷贝函数
 MyInt::MyInt(const MyInt& temp)
 {
 	this->m_Num = new int(*temp.m_Num);
+	cout << "拷贝构造" << endl;
 }
 
 //重载赋值符号
@@ -30,7 +31,7 @@ MyInt& MyInt::operator=(MyInt& p)
 			delete m_Num;
 			m_Num = NULL;
 		}
-		m_Num = new int(*p.m_Num);
+		this->m_Num = new int(*p.m_Num);
 		return *this;
 	}
 
@@ -44,4 +45,14 @@ MyInt::~MyInt()
 		delete m_Num;
 		m_Num = NULL;
 	}
+	cout << "析构函数" << endl;
 }
+
+//前置自增
+MyInt& MyInt::operator++()
+{
+	m_Num = m_Num + 1;
+	return *this;
+}
+
+//前置自减
