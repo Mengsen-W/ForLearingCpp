f684ecc841889a89da11b02c1dc2f3244a100739
Mengsen-W <mengsen_wang@163.com>
2020-02-03 19:33:30 +0800

    new

--- DataStructuresAlgorithm/DynamicProgramming.cpp
+++ DataStructuresAlgorithm/DynamicProgramming.cpp
@@ -2,7 +2,7 @@
  * @Author: Mengsen.Wang
  * @Date: 2020-02-03 14:48:07
  * @Last Modified by: Mengsen.Wang
- * @Last Modified time: 2020-02-03 15:39:23
+ * @Last Modified time: 2020-02-03 17:42:13
  */
 
 #include <vector>
@@ -62,6 +62,39 @@ double evalC(int n) {
     }
   }
   return c[n];
+}
+
+/**
+ * 计算矩阵乘法的最优排列
+ * c 包含 n 个矩阵每个矩阵的列数
+ * c[0] 为矩阵1的行数
+ * 乘法的最少次数置于 m[l][n] 中
+ * 具体的排序通过使用 lastChange 的另外过程来计算
+ * m 和 lastChange 的下标从 1 开始，而不是从 0 开始
+ * 注意： m 和 lastChange 主对角线下方的元素无意义
+ * 不用初始化
+ */
+void optMatrix(const vector<int> &c, vector<vector<int> > &m,
+               vector<vector<int> > &lastChange) {
+  static const int INFINITY = 10000000;
+  int n = c.size() - 1;
+
+  for (int left = 1; left <= n; ++left) m[left][left] = 0;
+
+  for (int k = 1; k < n; ++k)  // k 为 right - left
+    for (int left = 1; left <= n - k; ++left) {
+      // 对每一个位置进行
+      int right = left + k;
+      m[left][right] = INFINITY;
+      for (int i = left; i < right; ++i) {
+        int thisCost =
+            m[left][i] + m[i + 1][right] + c[left - 1] * c[i] * c[right];
+        if (thisCost < m[left][right]) {
+          m[left][right] = thisCost;
+          lastChange[left][right] = i;
+        }
+      }
+    }
 }
 
 int main() {
